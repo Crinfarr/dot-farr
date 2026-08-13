9b09cb73215b0a22208b1827588cc88a4815d16c
Asher Hoeveler <ahoeveler@apotechgroup.com>
2026-08-12 19:07:09 -0500

    add my work monitors lmao

--- hypr/monitors.lua
+++ hypr/monitors.lua
@@ -1,4 +1,22 @@
 hl.monitor({
+    output = "desc:Hewlett Packard HP E222 CNK53312QC",
+    mode = "preferred",
+    position = '0x0'
+})
+hl.monitor({
+    output = "desc:Hewlett Packard HP E222 CNK53312Q9",
+    mode = "preferred",
+    position = "auto-center-right",
+    transform = 1
+})
+hl.monitor({
+    output = "desc:Hewlett Packard HP E222 CNK53312QV",
+    mode = "preferred",
+    position = "auto-center-left",
+    transform = 1
+})
+
+hl.monitor({
     output = "DP-5",
     mode = "2560x1440",
     bitdepth = 10,
