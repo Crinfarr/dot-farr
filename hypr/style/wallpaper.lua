708144f26e7b35894a8ddf190179d08320ee9fd5
Asher Hoeveler <ahoeveler@apotechgroup.com>
2026-08-12 20:07:41 -0500

    it took just as long to hash check as it did to run ffmpeg

--- hypr/style/wallpaper.lua
+++ hypr/style/wallpaper.lua
@@ -57,22 +57,5 @@ hl.on("hyprland.start", function()
     gen_spanning_wp("~/.config/hypr/wallpaper.jxl")
 end)
 hl.on("config.reloaded", function()
-    print("Checking if wallpaper.jxl has changed")
-    local handle = io.popen("sha256sum -c ~/.config/hypr/run/.wp_sum > /dev/null; echo $?")
-    assert(handle, "failed to run sha256sum")
-    if handle:read("l") == "0" then
-    -- if false then --DEBUG
-        print("Wallpaper unchanged")
-    else
-        print("Wallpaper changed")
-        handle:close()
-        print("creating .wp_sum")
-        handle = io.popen("sha256sum -b ~/.config/hypr/wallpaper.jxl > ~/.config/hypr/run/.wp_sum")
-        assert(handle, "failed to run sha256sum")
-        handle:close()
-        print("generating wallpaper crops")
-        gen_spanning_wp("~/.config/hypr/wallpaper.jxl")
-        return
-    end
-    handle:close()
+    gen_spanning_wp("~/.config/hypr/wallpaper.jxl")
 end)
